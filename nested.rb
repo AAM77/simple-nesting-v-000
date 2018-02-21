5e941038b7586d9e5f4b93a05be51dfd5d776ef7
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-21 21:57:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 
+
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
@@ -131,6 +131,6 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 
 end
