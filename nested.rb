2249c540d9360652250fdc4cc04095059c6d0844
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-21 22:38:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,6 +87,7 @@ def adding_matz
 		 # At least two possible solutions:
 		 #programmer_hash.merge :yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}
 		 # OR:
+		 
 		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 		 programmer_hash
 
